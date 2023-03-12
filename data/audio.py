86165c52ae3257d380902804de913f1d8d75ebdb
Axel <axelfernandezcurros@gmail.com>
2023-03-12 02:52:27 +0100

    Update audio.py

--- data/audio.py
+++ data/audio.py
@@ -1,3 +1,31 @@
+def all_wav_to_mp3():
+    pass
+    '''bash
+        find . -type f -name "*.wav" -exec bash -c 'ffmpeg -i "$0" -codec:a libmp3lame -qscale:a 2 "${0%.wav}.mp3" && rm "$0"' {} \;
+    '''
+    '''Windows
+        Get-ChildItem -Recurse -Filter *.wav | ForEach-Object {
+        $newPath = $_.FullName.Replace(".wav", ".mp3")
+        ffmpeg -i $_.FullName -codec:a libmp3lame -qscale:a 2 $newPath
+        Remove-Item $_.FullName
+    }
+    '''
+
+
+def all_mp3_to_wav():
+    pass
+    '''bash
+        find . -type f -name "*.mp3" -exec bash -c 'ffmpeg -i "$0" "${0%.mp3}.wav" && rm "$0"' {} \;
+    '''
+    '''Windows
+        Get-ChildItem -Recurse -Filter *.mp3 | ForEach-Object {
+        $newPath = $_.FullName.Replace(".mp3", ".wav")
+        ffmpeg -i $_.FullName $newPath
+        Remove-Item $_.FullName
+    }
+    '''
+
+
 audio_extensions = {
     "WAV": ".wav",
     "MP3": ".mp3",
